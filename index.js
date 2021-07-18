9f1d877cea2b540a1007aa7a9c7fd82a98f4361f
eksant <eksant@gmail.com>
2021-07-18 23:17:01 +0800

    Implement queue for crawling website

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const request = require("request");
 const cheerio = require("cheerio");
+const Queue = require("bull");
 
-const url = "https://www.kompas.com";
-
-request(url, (err, res, body) => {
-  if (err && res.statusCode !== 200) throw err;
-
-  const result = [];
-  let $ = cheerio.load(body);
-  $(".article__list").each((i, val) => {
-    const title = $(val).find($(".article__title")).text().trim();
-    const link = $(val).find($(".article__link")).attr("href");
-    const imgUrl = $(val)
-      .children(".article__list__asset")
-      .children(".article__asset")
-      .find("a > img")
-      .attr("data-src");
-    const tag = $(val).find(".article__subtitle").text().trim();
-    const createdAt = $(val).find(".article__date").text().trim();
-
-    const meta = { title, link, imgUrl, tag, createdAt };
-    result.push(meta);
+async function main(baseurl = "https://seorangeksa.com") {
+  // Initiating the Queue
+  const crawlQueue = new Queue("CrawlQueue", {
+    redis: { host: "127.0.0.1", port: 6379 },
   });
 
-  console.log("RESULT ==> ", result);
-  console.log("TOTAL RESULT: ", result?.length);
-});
+  // Adding a job to the Queue
+  crawlQueue.add({ url: baseurl }, { delay: 6000, attempts: 2 });
+
+  // Consumer
+  crawlQueue.process(async (job) => {
+    const result = await crawl(job.data.url);
+    console.log("Result: ", result);
+  });
+}
+
+/**
+ * Crawling a web site
+ * @param baseurl : string
+ * @returns object with data (array) and total (int)
+ */
+async function crawl(baseurl) {
+  return new Promise((resolve, reject) => {
+    request(baseurl, (err, res, body) => {
+      if (err && res.statusCode !== 200) {
+        reject(err);
+        return;
+      }
+
+      const data = [];
+      let $ = cheerio.load(body);
+      $(".article__list").each((i, val) => {
+        const title = $(val)?.find($(".article__title")).text().trim();
+        const link = $(val)?.find($(".article__link")).attr("href");
+        const imgUrl = $(val)
+          ?.children(".article__list__asset")
+          .children(".article__asset")
+          .find("a > img")
+          .attr("data-src");
+        const tag = $(val)?.find(".article__subtitle").text().trim();
+        const createdAt = $(val)?.find(".article__date").text().trim();
+
+        data.push({ title, link, imgUrl, tag, createdAt });
+      });
+
+      resolve({ data, total: data.length });
+    });
+  });
+}
+
+const url = "https://www.kompas.com";
+main(url);
